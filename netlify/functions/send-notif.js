87fb8ca21620ac5e66da523707a1cd562d999308
Aurelie2205 <aurelmatt@icloud.com>
2026-06-06 22:05:54 +0200

    Update send-notif.js

--- netlify/functions/send-notif.js
+++ netlify/functions/send-notif.js
@@ -2,18 +2,17 @@ exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') return { statusCode: 405 };
   const { title, message } = JSON.parse(event.body);
   try {
-    const res = await fetch('https://api.onesignal.com/notifications', {
+    const res = await fetch('https://onesignal.com/api/v1/notifications', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Key os_v2_app_p236cc4dwfg2pn2uy477pfzjpfbe7bf6eg5utffey4rphyjkspgatr4mudvu7jrvawqidtbnkxeqdwmicfa4bgbukc6oyyz6rjht3fa'
+        'Authorization': 'Key ufgv3mmcmevsfwhjqbjplt6ld'
       },
       body: JSON.stringify({
         app_id: '7eb7e10b-83b1-4da7-b754-c73ff7972979',
-        target_channel: 'push',
         included_segments: ['All'],
-        headings: { en: title || 'DMC Event ✦' },
-        contents: { en: message || 'Nouveau contenu disponible' },
+        headings: { fr: title || 'DMC Event ✦', en: title || 'DMC Event ✦' },
+        contents: { fr: message || 'Nouveau contenu disponible', en: message || 'Nouveau contenu disponible' },
         url: 'https://dmc-event-v2.netlify.app'
       })
     });
